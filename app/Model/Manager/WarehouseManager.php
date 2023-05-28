7b2191bdf2d76a4ab342d144c11d08694aab22e4
vojtech <sopajyt@gmail.com>
2023-05-28 23:18:01 +0200

    adding stuff

---
+++ app/Model/Manager/WarehouseManager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model\Manager;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class WarehouseManager
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    
+}
